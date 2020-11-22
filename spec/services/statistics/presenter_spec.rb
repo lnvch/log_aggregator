642b5d6ffc5d27b6fb499abfe81135bae707cd0b
AliakseiLeanovich <aliaksei.leanovich@gmail.com>
2020-11-22 11:52:08 +0300

    Add statistics presenter spec

---
+++ spec/services/statistics/presenter_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require_relative '../../../services/statistics/presenter'
+require_relative '../../../models/statistics/result'
+
+RSpec.describe Statistics::Presenter do
+  subject { described_class }
+
+  let(:result) do
+    Statistics::Result.new(
+      [['/contact', 5], ['/about', 7], ['/help_page/1', 3]],
+      [['/contact', 2], ['/about', 1], ['/help_page/1', 3]]
+    )
+  end
+  let(:expected_output) do
+    <<-STR
+    /about 7 visits
+    /contact 5 visits
+    /help_page/1 3 visits
+
+    /help_page/1 3 unique views
+    /contact 2 unique views
+    /about 1 unique views
+    STR
+  end
+
+  it 'represents statistics result in defined order' do
+    expect { subject.call(result) }.to output(expected_output).to_stdout
+  end
+end
